e2a929d1624386b38dce6a424cd00bd6fb6b5090
Code Dude <dude@stokedbits.com>
2017-11-25 08:24:59 -0800

    Hello :)

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+import { HelloWorld } from 'cra-lib';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HelloWorld />
+      </div>
+    );
+  }
+}
+
+export default App;
